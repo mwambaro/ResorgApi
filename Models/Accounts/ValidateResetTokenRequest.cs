a4d9aeff826ad430df8457ba9942b0b28693de67
Obedom <onkezabahizi@gmail.com>
2020-07-20 21:29:24 +0200

    Accounts controller code. .gitignore file.

---
+++ Models/Accounts/ValidateResetTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResorgApi.Models.Accounts
+{
+    public class ValidateResetTokenRequest
+    {
+        [Required]
+        public string Token { get; set; }
+    }
+}
