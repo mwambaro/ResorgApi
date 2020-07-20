b61385cb4d9b6d3a7e2265f953f9aa0d552017f8
Obedom <onkezabahizi@gmail.com>
2020-07-20 22:00:16 +0200

    LICENSE, README, and program web host defaults configuration.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ResorgApi
 {
@@ -20,7 +14,8 @@ namespace ResorgApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://localhost:44000");
                 });
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Research Resource Organization (Resorg) API
+
+ASP.NET Core 3.1 - Resorg API with Email Sign Up, Verification, Authentication & Forgot Password
